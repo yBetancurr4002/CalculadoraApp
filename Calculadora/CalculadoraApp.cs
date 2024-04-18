6443b78549268dc51a0433dac37baa2be444d68a
yBetancurr4002 <147186691+yBetancurr4002@users.noreply.github.com>
2024-04-17 19:30:09 -0500

    CalculadoraApp

---
+++ Calculadora/CalculadoraApp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class CalculadoraApp
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("El divisor no puede ser cero.");
+            }
+
+            return (double)a / b;
+        }
+
+        public double SacarPorcentaje(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("El divisor no puede ser cero.");
+            }
+            var result = ((double)a / b) * 100;
+            return result;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,34 @@
+﻿using Calculadora;
+
+Console.WriteLine("Mi calculadora");
+
+var calcular = new CalculadoraApp();
+
+// declaración de variables
+int a = 5;
+int b = 10;
+
+// Sumar
+Console.WriteLine("SUMA");
+var suma = calcular.Sumar(a, b);
+Console.WriteLine($"La suma entre {a} y {b} es: {suma}\n");
+
+// Restar
+Console.WriteLine("RESTA");
+var resta = calcular.Restar(a, b);
+Console.WriteLine($"La resta entre {a} y {b} es: {resta}\n");
+
+// Multiplicar
+Console.WriteLine("MULTIPLICACIÓN");
+var multiplicacion = calcular.Multiplicar(a, b);
+Console.WriteLine($"La multiplicación entre {a} y {b} es: {multiplicacion}\n");
+
+// Dividir
+Console.WriteLine("DIVISIÓN");
+var division = calcular.Dividir(a, b);
+Console.WriteLine($"La división entre {a} y {b} es: {division}\n");
+
+// Calcular porcentaje
+Console.WriteLine("PORCENTAJE");
+var porcentaje = calcular.SacarPorcentaje(a, b);
+Console.WriteLine($"{a} es el {porcentaje}% de {b}\n");
